718bc15e40ca571449821a788d6294eea731c23b
ZacariasBendeck <ZacariasBendeck@users.noreply.github.com>
2015-08-03 19:44:28 -0600

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -6,13 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+
+drop database if exists tournament;
+
 create database tournament;
+
+\c tournament
 
 create table players(int serial, name text, wins int, matches int);
 
 create table matches(winner int, loser int);
-
-create table already_played(name text, played text);
 
 
 
